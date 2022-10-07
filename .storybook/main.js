b057c01bc3f227a75efb04ceb0f3f7b4a109d86f
ncdc-TakahiroArai <takahiro.arai@ncdc.co.jp>
2022-10-07 12:30:25 +0900

    add

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -8,5 +10,17 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@": path.resolve(__dirname, "../src"),
+        },
+      },
+    };
+  },
 }
